c26d44164eefbe759e7fe635210f1ef3047c2616
LuisGtz9610 <luisen9608@gmail.com>
2024-07-21 07:12:52 -0700

    Crear Proyecto

---
+++ SistemadeInventarioWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemadeInventarioWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
